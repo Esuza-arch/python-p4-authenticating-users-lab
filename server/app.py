df5b133d6fdaab96465aff9a03df05d5db8e402a
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2025-01-23 21:32:59 +0300

    Complete lab

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, request, session
+from flask import Flask, session, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Article, User
+from models import db, User, Article
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -18,40 +18,39 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
 
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            return {'message': 'User not found'}, 404
+
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+
+class Logout(Resource):
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
+        session.pop('user_id', None)
         return {}, 204
 
-class IndexArticle(Resource):
-    
+class CheckSession(Resource):
     def get(self):
-        articles = [article.to_dict() for article in Article.query.all()]
-        return articles, 200
+        user_id = session.get('user_id')
+        if not user_id:
+            return {}, 401
 
-class ShowArticle(Resource):
+        user = User.query.get(user_id)
+        if not user:
+            return {}, 404
 
-    def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
+        return user.to_dict(), 200
 
-        if session['page_views'] <= 3:
-
-            article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
-
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
